757fffbfe0d241f4b41df0e4f8ab7135fb2bd4c9
realChriss <automatikz1234@gmail.com>
2025-03-14 00:15:09 +0000

    style: format code

--- scripts/dbCheck.js
+++ scripts/dbCheck.js
@@ -6,8 +6,8 @@ const fs = require("fs");
 async function insertInitialData(client, data) {
   if (data.warn_type && data.warn_type.length > 0) {
     const warnTypeValues = data.warn_type
-      .map(w => `(${w.id}, '${w.name}', '${w.description}')`)
-      .join(',');
+      .map((w) => `(${w.id}, '${w.name}', '${w.description}')`)
+      .join(",");
     await client.query(`
       INSERT INTO public.warn_type (id, name, description) 
       VALUES ${warnTypeValues};
@@ -16,8 +16,8 @@ async function insertInitialData(client, data) {
 
   if (data.config && data.config.length > 0) {
     const configValues = data.config
-      .map(c => `(${c.id}, ${c.app_open}, ${c.send_wlc_msg})`)
-      .join(',');
+      .map((c) => `(${c.id}, ${c.app_open}, ${c.send_wlc_msg})`)
+      .join(",");
     await client.query(`
       INSERT INTO public.config (id, app_open, send_wlc_msg) 
       VALUES ${configValues};
@@ -34,21 +34,21 @@ async function checkAndInitializeDB() {
     await client.connect();
 
     const res = await client.query(
-      "SELECT count(*) FROM information_schema.tables WHERE table_schema = 'public';"
+      "SELECT count(*) FROM information_schema.tables WHERE table_schema = 'public';",
     );
     const tableCount = parseInt(res.rows[0].count, 10);
 
     if (tableCount === 0) {
       console.log("Database is empty. Running prisma db push...");
-      
+
       const dbPushPath = path.join(__dirname, "../docker/db-push.sh");
       try {
-        execSync(`bash ${dbPushPath}`, { stdio: 'inherit' });
+        execSync(`bash ${dbPushPath}`, { stdio: "inherit" });
         console.log("Database schema created. Inserting initial data...");
 
         const queriesPath = path.join(__dirname, "init.json");
-        const initialData = JSON.parse(fs.readFileSync(queriesPath, 'utf8'));
-        
+        const initialData = JSON.parse(fs.readFileSync(queriesPath, "utf8"));
+
         await insertInitialData(client, initialData);
         console.log("Initial data inserted successfully.");
       } catch (error) {
--- src/bot/slash/changelog.ts
+++ src/bot/slash/changelog.ts
@@ -109,7 +109,7 @@ const command: ClientSlash = {
       await interaction.reply({
         embeds: [errorEmbed.getEmbed()],
       });
-      
+
       return;
     }
 
