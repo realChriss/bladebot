56fdb7c0e4892b6809a75d649b3d7ed1b143088a
realChriss <automatikz1234@gmail.com>
2025-05-23 14:57:26 +0000

    chore: add tryout emoji asset

--- src/bot/assets/emoji.ts
+++ src/bot/assets/emoji.ts
@@ -1,6 +1,7 @@
 const Emoji: Record<string, string> = {
   pulse: "<:pulse:1357283254100688947>",
   waitlist: "<:waitlist:1360251417423777824>",
+  tryout: "<:tryout:1375487628207718480>",
 };
 
 export default Emoji;
